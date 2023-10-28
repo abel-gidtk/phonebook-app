656cf9c84f7fe48f093c352573067f9fe032dd87
Abel Gideon <abelgid29@gmail.com>
2023-10-28 19:09:58 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const cors = require('cors')
+
+app.use(cors())
+const app = express()
+app.use(express.json())
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(p => p.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        response.status(400).end()
+    }
+    persons.forEach(person => {
+        if (person.name === body.name) {
+            response.status(400).json({
+                error: 'name already exists'
+            })
+        }
+    })
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: Math.floor(Math.random() * 10000)
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
+})
+
+app.get('/info', (request, response) => {
+    response.send(`Phonebook has ${persons.length} persons`)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT)
